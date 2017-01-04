532f59526c8e3f16d89c5bf2955bd24e54a75f3b
Feifei GU <gufeifei@outlook.com>
2017-01-04 22:50:49 +1300

    Update HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from './../models';
+import { AlbumService } from './../services/album/album.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private albums: Album[] = [];
+  constructor(private _albumService: AlbumService) { }
 
   ngOnInit() {
+    this.getPopularAlbums();
   }
+
+  getPopularAlbums() {
+    this._albumService.getPopularAlbums()
+      .subscribe(albums => this.albums = albums)
+  };
+
 
 }
