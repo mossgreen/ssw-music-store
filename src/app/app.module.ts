11329e7b1ba02db567a51af385204af05a5ca8ea
Feifei GU <gufeifei@outlook.com>
2017-01-04 21:46:48 +1300

    ng generate component home --spec false

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,10 +27,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
