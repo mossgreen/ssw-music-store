473992a1416a9c0e046e1d49494c544ad2b68e54
Feifei GU <gufeifei@outlook.com>
2017-01-11 23:19:57 +1300

    Update src\app\genre-detail\genre-detail.component.ts

--- src/app/genre-detail/genre-detail.component.ts
+++ src/app/genre-detail/genre-detail.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Album } from './../models';
+import { GenreService } from './../services/genre/genre.service';
 
 @Component({
   selector: 'app-genre-detail',
   templateUrl: './genre-detail.component.html',
   styleUrls: ['./genre-detail.component.css']
 })
-export class GenreDetailComponent implements OnInit {
 
-  constructor() { }
+export class GenreDetailComponent implements OnInit, OnDestroy {
+  private paramSub: any;
+  private albums: Album[];
+
+  constructor(private _genreService:GenreService, private _router:Router, private _route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.paramSub.unsubscribe();
+  }
+
+  getGenreAlbums(name:string){
+    this._genreService.getGenreAlbums(name)
+    .subscribe(albums =>
+      this.albums = albums)
+  };
+
+  goToAlbumDetail(album:Album){
+    this._router.navigate([`/album/${album.albumId}`]);
   }
 
 }
