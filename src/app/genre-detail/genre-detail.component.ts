1b8eb4f57ea82fd9e892590d391eb979c2e09626
Feifei GU <gufeifei@outlook.com>
2017-01-11 23:24:02 +1300

    Bug fixed on src/app/genre-detail/genre-detail.component.ts

--- src/app/genre-detail/genre-detail.component.ts
+++ src/app/genre-detail/genre-detail.component.ts
@@ -13,19 +13,26 @@ export class GenreDetailComponent implements OnInit, OnDestroy {
   private paramSub: any;
   private albums: Album[];
 
-  constructor(private _genreService:GenreService, private _router:Router, private _route:ActivatedRoute) { }
+  constructor(private _genreService: GenreService, private _router: Router, private _route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.paramSub = this._route.params.subscribe(params => {
+      let name = params['name'];
+      this.getGenreAlbums(name);
+    });
+  }
+
+  ngOnDestroy() {
     this.paramSub.unsubscribe();
   }
 
-  getGenreAlbums(name:string){
+  getGenreAlbums(name: string) {
     this._genreService.getGenreAlbums(name)
-    .subscribe(albums =>
-      this.albums = albums)
+      .subscribe(albums =>
+        this.albums = albums)
   };
 
-  goToAlbumDetail(album:Album){
+  goToAlbumDetail(album: Album) {
     this._router.navigate([`/album/${album.albumId}`]);
   }
 
