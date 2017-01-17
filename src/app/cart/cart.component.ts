8ed7ab82bbc37555fee381d6ec984c6b6fffd565
Feifei GU <gufeifei@outlook.com>
2017-01-17 22:44:03 +1300

    Update Cart template

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -4,8 +4,8 @@ import {CartService} from '../services/cart/cart.service';
 
 @Component({
   selector: 'app-cart',
-  templateUrl: 'cart.component.html',
-  styleUrls: ['cart.component.css']
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
   cart: Cart = <Cart>{};
