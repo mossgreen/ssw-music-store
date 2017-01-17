15d4e895985ef1039670e0f0aa3ff00940bb427c
Feifei GU <gufeifei@outlook.com>
2017-01-17 23:16:24 +1300

    Adding item to cart

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Album } from '../models';
+import {AuthService} from '../services/auth/auth.service';
+
 
 @Component({
   selector: 'app-album-detail',
@@ -11,10 +13,12 @@ export class AlbumDetailComponent {
   @Input() album: Album;
   @Output('selected') selected = new EventEmitter();
 
+  constructor(private _auth: AuthService){}
+
   onSelect(album) {
     this.selected.next(album);
   }
   loggedIn() {
-    return false;
+    return this._auth.authenticated();
   }
 }
