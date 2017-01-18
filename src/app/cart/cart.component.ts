2f99099405b5194d6f71a06672fe22ecea2eb791
Feifei GU <gufeifei@outlook.com>
2017-01-18 23:21:01 +1300

    Bug fixed with Genres

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -15,6 +15,7 @@ export class CartComponent implements OnInit {
 
   ngOnInit() {
     this.getCart();
+    alert('hi');
   }
 
   getCart() {
--- src/app/genres/genres.component.ts
+++ src/app/genres/genres.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {GenreService} from './../services/genre/genre.service';
-import {Genre} from './../models';
+import { Router } from '@angular/router';
+import { GenreService } from './../services/genre/genre.service';
+import { Genre } from './../models';
 
 @Component({
   selector: 'app-genres',
@@ -10,27 +10,27 @@ import {Genre} from './../models';
 })
 export class GenresComponent implements OnInit {
 
-  public genres:Genre[];
-  isLoading:boolean;
+  public genres: Genre[];
+  isLoading: boolean;
 
-  constructor(private _genreService:GenreService, private _router:Router) { }
+  constructor(private _genreService: GenreService, private _router: Router) { }
 
   ngOnInit() {
     this.getGenres();
     this._router.navigate([`/genres/Pop`]);
   }
 
-  getGenres(){
+  getGenres() {
     this.genres = [];
     this.isLoading = true;
     this._genreService.getGenres()
-    .subscribe(genres => {
-      this.genres = genres;
-      this.isLoading = false;
-    });
+      .subscribe(genres => {
+        this.genres = genres;
+        this.isLoading = false;
+      });
   }
 
-goToGenre(genre:Genre){
-  this._router.navigate([`/genres/$genre.name`]);
-}
+  goToGenre(genre: Genre) {
+    this._router.navigate([`/genres/${genre.name}`]);
+  }
 }
